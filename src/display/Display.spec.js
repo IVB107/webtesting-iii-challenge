5181296d6a70710b490722f5c838e54a7069ff67
IVB107 <ianvb107@gmail.com>
2019-04-11 20:33:43 -0500

    Completed tests for Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -13,38 +13,59 @@ describe('Controls', () => {
     render(<Display />);
   });
 
-  const appTree = (
-    <Dashboard >
-      <Display />
-      <Controls />
-    </Dashboard>
-  );
+  // const appTree = (
+  //   <Dashboard >
+  //     <Display />
+  //     <Controls />
+  //   </Dashboard>
+  // );
 
-  it('Displays if gate is open/closed and if it is locked/unlocked', () => {
-    // const { getByText } = render(appTree);
-    const { queryByText, rerender } = render(<Display locked={false} closed={false} />);
-
-    expect(queryByText(/closed/i)).toBeNull();
-    rerender(<Display closed={true} />);
-    expect(queryByText(/open/i)).toBeNull();
-
-  });
+  describe('Display Text', () => {
+    
+    it('Displays if gate is open/closed and if it is locked/unlocked', () => {
+      const { queryByText, rerender } = render(<Display closed={false} />);
   
-  it('Displays "Closed" if the closed prop is true and "Open" if otherwise', () => {
-    // Test stuff
-  });
-
-  it('Displays "Locked" if the locked prop is true and "Unlocked" if otherwise', () => {
-    // Test stuff
-  });
-
-  it('Should use the red-led class when locked or closed.', () => {
-    // Test stuff
-  });
-
-  it('Should use the green-led class when unlocked or open.', () => {
-    // Test stuff
-  });
+      expect(queryByText(/closed/i)).toBeNull();
+      rerender(<Display closed={true} />);
+      expect(queryByText(/open/i)).toBeNull();
+    });
+    
+    it('Displays "Closed" if the closed prop is true and "Open" if otherwise', () => {
+      const { queryByText, rerender } = render(<Display closed={true} />);
   
+      expect(queryByText(/closed/i)).toBeTruthy();
+      rerender(<Display closed={false} />);
+      expect(queryByText(/open/i)).toBeTruthy();
+    });
+  
+    it('Displays "Locked" if the locked prop is true and "Unlocked" if otherwise', () => {
+      const { getByText, rerender } = render(<Display locked={true} />);    
+      
+      expect(getByText(/locked/i)).toHaveTextContent('Locked');
+      rerender(<Display locked={false} />);
+      expect(getByText(/locked/i)).toHaveTextContent('Unlocked');
+    });
+
+  });
+
+  describe('Display Colors', () => {
+
+    it('Should use the red-led class when locked or closed.', () => {
+      const { getByText, rerender } = render(<Display locked={true} />);
+      
+      expect(getByText(/locked/i)).toHaveClass('red-led');
+      rerender(<Display locked={false} />);
+      expect(getByText(/locked/i)).toHaveClass('green-led');
+    });
+    
+    it('Should use the green-led class when unlocked or open.', () => {
+      const { getByText, rerender } = render(<Display closed={true} />);
+      
+      expect(getByText(/closed/i)).toHaveClass('red-led');
+      rerender(<Display locked={false} />);
+      expect(getByText(/open/i)).toHaveClass('green-led');
+    });
+    
+  });
 
 })
